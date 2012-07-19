482c4228ae313a3b6821a4334190ae3fc1c309f1
vzever <weizhe86@gmail.com>
2012-07-19 22:50:11 +0800

    init

---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import socket,select
+
+
+
+def main():
+    host='127.0.0.1'
+    port=23
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((host,port))
+    s.listen(5)
+    socklist=[s]
+    while True:
+        listening(socklist)
+def listening(socklist):
+    rlist,wlist,elist=select.select(socklist,[],[],0)
+    for sock in rlist:
+        if sock==socklist[0]:
+            print 's.accept',socklist
+            clientsock,clientaddr = sock.accept()
+            socklist.append(clientsock)
+        else:
+            words=sock.recv(8196)
+            print [words]
+            sendall(socklist,words,sock)
+def sendall(socklist,line,tsock):
+    if '\xff' in line:return#对telnet命令符号过滤
+    for sock in socklist[1:]:
+        if sock!=tsock:
+            sock.send(line)
+if __name__ == "__main__":
+    main()
+
